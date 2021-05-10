03eb6df322edcaea6d502efb7f7094bfe4bc712a
Robin Soo-hyuk Nam <shnam7@gmail.com>
2021-05-10 11:55:48 +0900

    Add time conversion functions

--- include/gtime.h
+++ include/gtime.h
@@ -13,10 +13,10 @@
 namespace gcl
 {
 
-typedef uint64_t tick_t;   // timer tick count type
-typedef uint64_t nsec_t;   // nanosec time type
-typedef unsigned usec_t;   // microsec time type
-typedef unsigned msec_t;   // millisec time type
+typedef uint64_t        tick_t;   // timer tick count type
+typedef uint64_t        nsec_t;   // nanosec time type
+typedef unsigned long   usec_t;   // microsec time type
+typedef unsigned int    msec_t;   // millisec time type
 
 //--- get time elapsed since system start in nanosec
 gcl_api tick_t ticks();
@@ -26,8 +26,29 @@ inline usec_t ticks_to_usec(tick_t ticks) { return (usec_t)(ticks_to_nsec(ticks)
 inline msec_t ticks_to_msec(tick_t ticks) { return (msec_t)(ticks_to_nsec(ticks) / 1000000); }
 
 inline tick_t nsec_to_ticks(nsec_t nsec) { return (tick_t)nsec; }
+inline usec_t nsec_to_usec(nsec_t nsec) { return (usec_t)(nsec / 1000); }
+inline msec_t nsec_to_msec(nsec_t nsec) { return  (msec_t)(nsec / 1000000); }
+
 inline tick_t usec_to_ticks(usec_t usec) { return nsec_to_ticks(((nsec_t)usec) * 1000); }
+inline nsec_t usec_to_nsec(usec_t usec) { return ((nsec_t)usec) * 1000; }
+inline msec_t usec_to_msec(usec_t usec) { return (msec_t)(usec / 1000); }
+
 inline tick_t msec_to_ticks(msec_t msec) { return nsec_to_ticks(((nsec_t)msec) * 1000000); }
+inline nsec_t msec_to_nsec(msec_t msec) { return  ((usec_t)msec) * 1000000; }
+inline usec_t msec_to_usec(msec_t msec) { return  ((usec_t)msec) * 1000; }
+
+inline msec_t t2m(tick_t ticks) { return ticks_to_msec(ticks); }
+inline nsec_t t2n(tick_t ticks) { return ticks_to_nsec(ticks); }
+inline usec_t t2u(tick_t ticks) { return ticks_to_usec(ticks); }
+inline tick_t n2t(nsec_t nsec) { return nsec_to_ticks(nsec); }
+inline usec_t n2u(nsec_t nsec) { return nsec_to_usec(nsec); }
+inline msec_t n2m(nsec_t nsec) { return nsec_to_msec(nsec); }
+inline tick_t u2t(usec_t usec) { return usec_to_ticks(usec); }
+inline nsec_t u2n(usec_t usec) { return usec_to_nsec(usec); }
+inline msec_t u2m(usec_t usec) { return usec_to_msec(usec); }
+inline tick_t m2t(msec_t msec) { return msec_to_ticks(msec); }
+inline nsec_t m2n(msec_t msec) { return msec_to_nsec(msec); }
+inline usec_t m2u(msec_t msec) { return msec_to_usec(msec); }
 
 inline tick_t elapsed_ticks(tick_t tm) { return ticks() - tm; }
 inline nsec_t elapsed_nsec(tick_t tm) { return ticks_to_nsec(elapsed_ticks(tm)); }
--- samples/ex-time.cpp
+++ samples/ex-time.cpp
@@ -23,7 +23,7 @@ void precision_test()
         nsec_t elapsed = elapsed_nsec(tm1);
         nsec_t error = elapsed - time_to_wait;
 
-        printf("try[%02d] %lf(msec) actual:%zu(ns) %u(us) %u(ms) --> gap: %zu(ns), %zu(us) %zu(ms) [%.3lf%%]\n",
+        printf("try[%02d] %lf(msec) actual:%zu(ns) %lu(us) %u(ms) --> gap: %zu(ns), %zu(us) %zu(ms) [%.3lf%%]\n",
             i,
             ((double)time_to_wait)/1000000,
             ticks_to_nsec(elapsed), ticks_to_usec(elapsed), ticks_to_msec(elapsed),
@@ -33,8 +33,24 @@ void precision_test()
     }
 }
 
+void conversion_test()
+{
+    tick_t tm = ticks();
+    printf("ticks=%zu ns=%zu us=%lu ms=%u\n", tm, t2n(tm), t2u(tm), t2m(tm));
+
+    nsec_t ns = t2n(tm);
+    printf("ticks=%zu ns=%zu us=%lu ms=%u\n", n2t(ns), ns, n2u(ns), n2m(ns));
+
+    usec_t us = t2u(tm);
+    printf("ticks=%zu ns=%zu us=%lu ms=%u\n", usec_to_ticks(us), u2n(us), us, u2m(us));
+
+    msec_t ms = t2m(tm);
+    printf("ticks=%zu ns=%zu us=%lu ms=%u\n", m2t(ms), m2n(ms), m2u(ms), ms);
+}
+
 int main()
 {
-    precision_test();
+    // precision_test();
+    conversion_test();
     return 0;
 }
